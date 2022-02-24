af465655f4e54917ddd416c357ebd6965556d714
monaqa <cmonaqa@gmail.com>
2022-02-25 08:22:38 +0900

    add queries and corpus

---
+++ queries/highlights.scm
@@ -0,0 +1,48 @@
+[
+ "participant"
+ "sequenceDiagram"
+ "as"
+ "activate"
+ "deactivate"
+ "note"
+ "over"
+ "link"
+ "links"
+ ; "left of"
+ ; "right of"
+ "properties"
+ "details"
+ "title"
+ "loop"
+ "rect"
+ "opt"
+ "alt"
+ "else"
+ "par"
+ "and"
+ "end"
+ (stmt_autonumber)
+ (note_placement_left)
+ (note_placement_right)
+ ] @keyword
+
+[
+ ":"
+ (signal_plus_sign)
+ (signal_minus_sign)
+ ] @punctuation.delimiter
+
+[
+ (solid_arrow)
+ (dotted_arrow)
+ (solid_open_arrow)
+ (dotted_open_arrow)
+ (solid_cross)
+ (dotted_cross)
+ (solid_point)
+ (dotted_point)
+ ] @operator
+
+(actor) @field
+
+(directive) @include
